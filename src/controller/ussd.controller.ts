c6f555693c7dad273c1cf43b5093cef568a4ff83
tHEO2k17 <tpee935@gmail.com>
2021-02-25 20:10:33 +0000

    added vehicle model

--- src/controller/ussd.controller.ts
+++ src/controller/ussd.controller.ts
@@ -36,7 +36,7 @@ export default class UssdContoller {
     }
 
     private getVehicleInfo(res: any): string {
-        return `Driver: ${res.driver.name}\nVehicle Make: ${res.make}\nVehicle Type: ${res.type}\nStation: ${res.station}\nMunicipal Assembly: ${res.municipal_assembly}\n`;
+        return `Driver: ${res.driver.name}\nVehicle Make: ${res.make}\nVehicle Type: ${res.model}\nStation: ${res.station}\nMunicipal Assembly: ${res.municipal_assembly}\n`;
     }
 
     private divider = (): string => '\n-----------------\n';
