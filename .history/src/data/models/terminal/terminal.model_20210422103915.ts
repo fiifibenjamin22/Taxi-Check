31514a5281be662d2f7834b6cace24992d2c44cb
tHEO2k17 <tpee935@gmail.com>
2021-04-22 10:48:15 +0000

    added field for municipal assembly | refactored naming convention for other camel case fields

--- .history/src/data/models/terminal/terminal.model_20210422103915.ts
+++ .history/src/data/models/terminal/terminal.model_20210422103915.ts
@@ -4,7 +4,6 @@ let terminalSchema = new mongoose.Schema({
     terminal_name: { type: String, require: 'First name is Required' },
     contact: { phone_number: String, email: String },
     address: { physical_address: String, postal_address: String, ghana_post: String },
-    municipal_assembly_id: Schema.Types.ObjectId,
     terminal_master_id: Schema.Types.ObjectId,
     created_by: Schema.Types.ObjectId
 });
