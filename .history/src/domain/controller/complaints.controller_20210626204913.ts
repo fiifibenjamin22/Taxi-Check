04237fc2ef653650702b52eb4ceb4f640e7f86d2
tHEO2k17 <tpee935@gmail.com>
2021-06-26 20:50:10 +0000

    analytics

---
+++ .history/src/domain/controller/complaints.controller_20210626204913.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Query, Res, Route, Tags, Response, SuccessResponse, Post, TsoaResponse, Body, Delete, Path } from "tsoa";
+import logging from "../../core/utils/logging";
+import ComplaintsService from "../../data/services/complaints.service";
+import { IApiResponse, IErrorResponse } from "../interfaces/common/responses.interface";
+import { IComplaints } from "../interfaces/complaints.interface";
+
+const NAMESPACE = 'Complaints Controller';
+@Route("/api/complaints")
+@Tags('Complaints')
+export class AssemblyController extends Controller {
+
+    @Get('/all')
+    public async getAll(@Query() limit?: number, @Res() notFoundResponse?: TsoaResponse<404, IErrorResponse>): Promise<IApiResponse> {
+        logging.info(NAMESPACE, 'Get all complaints');
+
+        let complaints: any[] = await ComplaintsService.list(limit);
+        if (!complaints || complaints.length == 0) notFoundResponse(404, { message: "No records found" });
+
+        return { 'message': "Fetched", data: complaints };
+    }
+
+    @Response<IErrorResponse>(422, "Validation Failed")
+    @SuccessResponse("201", "Created")
+    @Post('/create')
+    public async create(@Body() complaints: IComplaints): Promise<void> {
+        logging.info(NAMESPACE, 'Create complaint');
+
+        this.setStatus(201);
+        return await ComplaintsService.create(complaints);
+    }
+    
+    @Delete('/delete/{complaintId}')
+    public async delete(@Path() complaintId: string): Promise<void> {
+        logging.info(NAMESPACE, 'Delete complaint');
+
+        return await ComplaintsService.deleteById(complaintId);
+    }
+}
