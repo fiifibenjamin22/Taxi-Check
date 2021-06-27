923a5059cfc6f00d01d3350ccbd558887c60f61f
tHEO2k17 <tpee935@gmail.com>
2021-06-27 17:48:08 +0000

    complaints controller

--- .history/src/domain/interfaces/complaints.interface_20210627173543.ts
+++ .history/src/domain/interfaces/complaints.interface_20210627173543.ts
@@ -1,8 +1,8 @@
 export interface IComplaints{
     vehicle_plate: string,
     reported_by: string,
-    assigned_to?: string,
-    reason?: string,
-    comments?: string,
+    assigned_to: string,
+    reason: string,
+    comments: string,
     status: "PENDING" | "ASSIGNED" | "ACTION_TAKEN",
 }
